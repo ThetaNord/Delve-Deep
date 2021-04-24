0c384a0f2dc8f302386454cbb79d4f1ae27e8a7c
Tuure Saloheimo <tuure.saloheimo@gmail.com>
2021-04-24 17:17:50 +0300

    Initial commit

---
+++ app/main.rb
@@ -0,0 +1,22 @@
+def tick(args)
+  args.outputs.labels << [640, 700, 'Delve Deep', 50, 1]
+  x_mid = ((args.grid.left+args.grid.right)/2).floor
+  y_mid = ((args.grid.top+args.grid.bottom)/2).floor
+  args.outputs.sprites << get_character_sprite(0, 20, x_mid, y_mid)
+end
+
+def get_character_sprite(idx, scale, x, y)
+  [
+    {
+      x: x-8*scale,
+      y: y-8*scale,
+      w: 16*scale,
+      h: 16*scale,
+      path: 'sprites/characters.png',
+      tile_x: (idx % 4) * 16,
+      tile_y: (idx / 4).floor * 16,
+      tile_w: 16,
+      tile_h: 16
+    }
+  ]
+end
